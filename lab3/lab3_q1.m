062bcab0201129cc14cf9a183ca130f2e172b69e
Jinming Wang <jinming4@cs.ualberta.ca>
2019-10-22 10:49:46 -0600

    lab3

--- lab3/lab3_q1.m
+++ lab3/lab3_q1.m
@@ -8,6 +8,7 @@ plot(Y1(1, :), Y1(2, :), '*')
 %plot(Y3(1, :), Y3(2, :), '.')
 
 [A_mean, A_normalized, D, V] = myPCA(Y1);
+plot(A_normalized(1,:), A_normalized(2,:), '.')
 
 function [A_mean, A_normalized, D, V] = myPCA(A)
     A_mean = mean(A, 2);
@@ -17,7 +18,7 @@ function [A_mean, A_normalized, D, V] = myPCA(A)
 
     [V, D] = eig(covar);
     
-    A_normalized = covar * A - A_mean * ones(1, size(A, 2));
+    A_normalized =  covar * centerA;
     
     hold on
     plot(centerA(1,:), centerA(2,:), 'g.');
